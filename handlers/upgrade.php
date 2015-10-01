2f885853647c3b451c2f56e97c0aa519853bf62a
jbroadway <johnny@johnnybroadway.com>
2015-10-01 14:40:13 -0500

    Upgrade script now applies updates recursively

--- handlers/upgrade.php
+++ handlers/upgrade.php
@@ -1,7 +1,7 @@
 <?php
 
 // keep unauthorized users out
-$this->require_admin ();
+$this->require_acl ('admin', $this->app);
 
 // set the layout
 $page->layout = 'admin';
@@ -11,48 +11,67 @@ $version = Appconf::get ($this->app, 'Admin', 'version');
 $current = $this->installed ($this->app, $version);
 
 if ($current === true) {
-	// app is already installed and up-to-date, stop here
-	$page->title = __ ('Already up-to-date');
-	printf ('<p><a href="/%s/admin">%s</a>', $this->app, __ ('Continue'));
-	return;
+    // app is already installed and up-to-date, stop here
+    $page->title = __ ('Already up-to-date');
+    printf ('<p><a href="/%s">%s</a>', Appconf::get ($this->app, 'Admin', 'handler'), __ ('Home'));
+    return;
 }
 
 $page->title = sprintf (
-	'%s: %s',
-	__ ('Upgrading App'),
-	Appconf::get ($this->app, 'Admin', 'name')
+    '%s: %s',
+    __ ('Upgrading App'),
+    Appconf::get ($this->app, 'Admin', 'name')
 );
 
-// grab the database driver and begin the transaction
+// grab the database driver
 $conn = conf ('Database', 'master');
 $driver = $conn['driver'];
-DB::beginTransaction ();
 
-// parse the database schema into individual queries
-$file = 'apps/' . $this->app . '/conf/upgrade_' . Appconf::get ($this->app, 'Admin', 'version') . '_' . $driver . '.sql';
-if (file_exists ($file)) {
-	$sql = sql_split (file_get_contents ($file));
+// get the base new version and current version for comparison
+$base_version = preg_replace ('/-.*$/', '', $version);
+$base_current = preg_replace ('/-.*$/', '', $current);
 
-	// execute each query in turn
-	foreach ($sql as $query) {
-		if (! DB::execute ($query)) {
-			// show error and rollback on failures
-			printf (
-				'<p class="visible-notice">%s: %s</p><p>%s</p>',
-				__ ('Error'),
-				DB::error (),
-				__ ('Install failed.')
-			);
-			DB::rollback ();
-			return;
-		}
-	}
+// find upgrade scripts to apply
+$files = glob ('apps/' . $this->app . '/conf/upgrade_*_' . $driver . '.sql');
+$apply = array ();
+foreach ($files as $k => $file) {
+    if (preg_match ('/^apps\/' . $this->app . '\/conf\/upgrade_([0-9.]+)_' . $driver . '\.sql$/', $file, $regs)) {
+        if (version_compare ($regs[1], $base_current, '>') && version_compare ($regs[1], $base_version, '<=')) {
+            $apply[$regs[1]] = $file;
+        }
+    }
 }
 
-// commit transaction and mark the app installed
+// begin the transaction
+DB::beginTransaction ();
+
+// apply the upgrade scripts
+foreach ($apply as $ver => $file) {
+    // parse the database schema into individual queries
+    $sql = sql_split (file_get_contents ($file));
+
+    // execute each query in turn
+    foreach ($sql as $query) {
+        if (! DB::execute ($query)) {
+            // show error and rollback on failures
+            printf (
+                '<p>%s</p><p class="visible-notice">%s: %s</p>',
+                __ ('Upgrade failed on version %s. Rolling back changes.', $ver),
+                __ ('Error'),
+                DB::error ()
+            );
+            DB::rollback ();
+            return;
+        }
+    }
+
+    // add any custom upgrade logic here
+}
+
+// commit the transaction
 DB::commit ();
+
+// mark the new version installed
 $this->mark_installed ($this->app, $version);
 
-printf ('<p><a href="/%s/admin">%s</a></p>', $this->app, __ ('Done.'));
-
-?>
+printf ('<p><a href="/%s">%s</a>', Appconf::get ($this->app, 'Admin', 'handler'), __ ('Done.'));
